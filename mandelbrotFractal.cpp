859ab898c6118fff33639013217a8c1455dc4162
shrishti0308 <125468465+shrishti0308@users.noreply.github.com>
2024-01-08 14:51:11 +0530

    Update mandelbrotFractal.cpp
    Better Code Readability

--- mandelbrotFractal.cpp
+++ mandelbrotFractal.cpp
@@ -2,7 +2,7 @@
 #include <numeric>
 #include <complex>
 
-// Mandelbrot Number is of the form "Z = Z**2 + c"
+// Mandelbrot Number is of the form "Z = Z**2 + c" or "Z = pow(Z,2) + c"
 int isInMandelbrotSet(std::complex<double> c){
     std::complex<double> z(0,0);
     for (int i = 0; i < 50; i++) {      // Change i < __ To Increase the Quality of render
@@ -10,8 +10,8 @@ int isInMandelbrotSet(std::complex<double> c){
 
         // Using Norm Function to check for the bounds, which returns the squared magnitude of the Complex number 'z'
         if(std::norm(z) > 16)           // 16 is completely arbitrary
-            return i;                   // Returning 'i' will give us # of Iterations it took to go out of the bounds of Mandelbrot Set, 
-                                        // which will decide the color of the pixel
+            return i;                   
+        // Returning 'i' will give us # of Iterations it took to go out of the bounds of Mandelbrot Set, which will decide the color of the pixel
     }
     return 0;
 }
@@ -24,7 +24,7 @@ int main(int argc, char const *argv[])
     SDL_CreateWindowAndRenderer(1000,1000,0, &window, &renderer);
 
     // Looping Over x & y, And it will check if the current comple number is a Maldelbrot Number or Not
-    // Manderbrot Number is of the form "Z = Z**2 + c"
+    // Manderbrot Number is of the form "Z = Z**2 + c" or "Z = pow(Z,2) + c"
     for (double x = 0.0; x < 1.0; x += 0.001){
         for(double y = 0.0; y < 1.0; y += 0.001){
             double pointX = std::lerp(-3.0, 3.0, x);     // Computes the Linear Interpolation b/w [a,b], which will give us the points we want to check for the x-axis
